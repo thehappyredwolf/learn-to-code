f06e48d323b0d009d4ca98cc9b645f0468b1d94d
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2025-12-17 00:28:17 +0200

    Add cbd18c5e-236f-497a-a42e-e83810cc0f04 solution

---
+++ 02-variables/cbd18c5e-236f-497a-a42e-e83810cc0f04.py
@@ -0,0 +1,12 @@
+game_one_score = 97
+game_two_score = 92
+game_three_score = 106
+game_four_score = 105
+
+# Don't touch above this line
+
+average_score = (game_one_score + game_two_score + game_three_score + game_four_score)  / 4
+
+# Don't touch below this line
+
+print(round(average_score))
