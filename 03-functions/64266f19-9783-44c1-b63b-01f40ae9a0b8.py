7df4d75922565e1b5679d61ad8b11824cfe29da5
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2025-12-17 00:39:09 +0200

    Add 64266f19-9783-44c1-b63b-01f40ae9a0b8 solution

---
+++ 03-functions/64266f19-9783-44c1-b63b-01f40ae9a0b8.py
@@ -0,0 +1,25 @@
+def triple_attack(damage_one, damage_two, damage_three):
+    return damage_one + damage_two + damage_three
+
+
+# Don't touch below this line
+
+# This is the first triple attack
+attack_one = 2
+attack_two = 4
+attack_three = 3
+first_triple_attack_damage = triple_attack(attack_one, attack_two, attack_three)
+
+print("Getting damage for", attack_one, attack_two, "and", attack_three, "...")
+print(first_triple_attack_damage, "points of damage dealt!")
+print("=====================================")
+
+# This is the second triple attack
+attack_four = -1
+attack_five = 10
+attack_six = 5
+second_triple_attack_damage = triple_attack(attack_four, attack_five, attack_six)
+
+print("Getting damage for", attack_four, attack_five, "and", attack_six, "...")
+print(second_triple_attack_damage, "points of damage dealt!")
+print("=====================================")
