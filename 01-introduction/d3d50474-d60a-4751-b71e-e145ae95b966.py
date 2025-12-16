9b436e354872a24b50959b610aa19b69c31bd970
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2025-12-16 23:47:19 +0200

    Add d3d50474-d60a-4751-b71e-e145ae95b966 solution

---
+++ 01-introduction/d3d50474-d60a-4751-b71e-e145ae95b966.py
@@ -0,0 +1,8 @@
+sword_damage = 10
+start_health = 100
+end_health = start_health - sword_damage
+
+# Don't touch below this line
+print(f"Sam's health is: {start_health}")
+print(f"Sam takes {sword_damage} damage...")
+print(f"Sam's health is: {end_health}")
