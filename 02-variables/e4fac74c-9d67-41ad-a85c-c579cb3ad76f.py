f574a0ced40848759a083c3d2d19b93f5c82ab1c
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2025-12-17 00:21:01 +0200

    Add e4fac74c-9d67-41ad-a85c-c579cb3ad76f solution

---
+++ 02-variables/e4fac74c-9d67-41ad-a85c-c579cb3ad76f.py
@@ -0,0 +1,10 @@
+quest_start = "You there! Adventurer!"
+quest_middle = "The local mine has been taken over by orcs!"
+quest_end = "We need your help taking it back."
+quest_objective = "Bring back 8 of their axes as proof of your hard work."
+space = " "
+
+# don't touch above this line
+print(quest_start)
+print(quest_middle)
+print(f"{quest_end}{space}{quest_objective}")
