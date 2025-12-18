8e40c13afd229d8fb3e6f4b4e6d4d5c105a2eb8f
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2025-12-18 13:24:35 +0200

    Add 3f2e485c-fcc2-419c-8664-f4b6318b4c7f solution

---
+++ 08-loops/3f2e485c-fcc2-419c-8664-f4b6318b4c7f.py
@@ -0,0 +1,31 @@
+def award_enchantments(start, end, step):
+    counter = 0
+    for quest_number in range(start, end, step):
+        counter += 1
+        if (counter < 3):
+            continue
+        if (counter == 3):
+            counter = 0
+        enchantment_strength = quest_number * 5
+        print(
+            f"Enchantment of strength {enchantment_strength} awarded for completing {quest_number} quests!"
+        )
+
+
+# Don't touch below this line
+
+
+def test(start, end, step):
+    print(f"Testing with quests {start} through {end - 1}:")
+    award_enchantments(start, end, step)
+    print("========================================")
+
+
+def main():
+    test(1, 11, 1)
+    test(20, 24, 1)
+    test(10, 12, 1)
+    test(11, 19, 1)
+
+
+main()
