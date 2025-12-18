763a84396b0e13dbb28c4cc18fafa0112c957e60
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2025-12-18 13:51:35 +0200

    Update 565dd496-0765-4e10-b074-85931fba340f solution

--- 08-loops/565dd496-0765-4e10-b074-85931fba340f/main.py
+++ 08-loops/565dd496-0765-4e10-b074-85931fba340f/main.py
@@ -1,9 +1,8 @@
 def calculate_experience_points(level):
     total_xp = 0
     
-    if (level == 1):
-        return total_xp
-    else:
+    if (level > 1):
         for i in range(1, level):
             total_xp += i * 5
+            
     return total_xp
